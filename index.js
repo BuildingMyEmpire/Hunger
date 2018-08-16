25edfdbe666b7949d62943daa749981b63783ac5
alicex28 <42008634+alicex28@users.noreply.github.com>
2018-08-16 14:41:19 -0400

    main page (collage, maps, signup, footer)

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 var elements = document.getElementsByClassName("column");
+
+var modal = document.querySelector(".modal");
+var closeButton = document.querySelector(".close-button");
+
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+        toggleModal();
+
+}
+
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("load", windowOnClick);
