7f76512029bf184403c7ad6a6713f40e7980d63c
BuildingMyEmpire <42008710+BuildingMyEmpire@users.noreply.github.com>
2018-08-17 14:55:07 -0400

    Add files via upload

---
+++ hugs.js
@@ -0,0 +1,26 @@
+var funFacts = [
+     ["Atlantic:<p>Population: 274,026</p><p>Food insecurity rate:12.9%</p>", 213,599],
+     ["Bergen:<p>Population: 905,116</p><p>Food insecurity rate: 7.5%</p>", 40.707213, 360-74.076559],
+     ["Burlington:<p>Population: 448,734</p><p>Food insecurity rate: 9.9%,</p>", 40.77, 360-74.03],
+     ["Camden:<p>Population: 513,657</p><p>Food insecurity rate: 12.2%,</p>", 40.761384, 360-74.035241],
+     ["Cape May:<p>Population: 92,265</p><p>Food insecurity rate: 12.4%,</p>", 40.772944, 360-74.143995],
+     ["Cumberland:<p>Population: 156,898</p><p>Food insecurity rate: 12.9%,</p>", 40.746583, 360-74.1568],
+     ["Essex:<p>Population: 783,969<p><p>Food insecurity rate: 17.1%,</p>", 40.797239, 360-74.012143],
+     ["Gloucester:<p>Population: 288,288</p><p>Food insecurity rate: 9.9%,</p>",40.739791, 360-74.04677],
+     ["Hudson:<p>Population: 634,266</p><p>Food insecurity rate: 10.5%,</p>", 40.739791, 360-74.04677],
+     ["Hunterdon:<p>Population: 128,349</p><p>Food insecurity rate: 6.3%,</p>", 40.766916, 360-74.146635],
+     ["Mercer:<p>Population: 366,513</p><p>Food insecurity rate: 11.0%,</p>", 40.708328, 360-74.087328],
+     ["Middlesex:<p>Population: 809,858</p><p>Food insecurity rate: 8.4%,</p>", 40.732661, 360-74.04137],
+     ["Monmouth:<p>Population: 630,380</p><p>Food insecurity rate: 8.5%,</p>", 40.74032, 360-74.031139],
+     ["Morris:<p>Population: 492,276</p><p>Food insecurity rate: 6.1%,</p>", 40.753079, 360-74.048066],
+     ["Ocean:<p>Population: 576,567</p><p>Food insecurity rate: 9.4%,</p>", 40.753079, 360-74.048066],
+     ["Passaic:<p>Population: 501,226</p><p>Food insecurity rate: 9.6%,</p>", 40.753079, 360-74.048066],
+     ["Salem:<p>Population: 66,083</p><p>Food insecurity rate: 13.0%,</p>", 40.753079, 360-74.048066],
+     ["Somerset:<p>Population: 323,444</p><p>Food insecurity rate: 6.9%,</p>", 40.753079, 360-74.048066],
+     ["Sussex:<p>Population: 149,265</p><p>Food insecurity rate: 7.2%,</p>", 40.753079, 360-74.048066],
+     ["Union:<p>Population: 536,499</p><p>Food insecurity rate: 9.6%,</p>", 40.753079, 360-74.048066],
+     ["Warren:<p>Population: 180,692</p><p>Food insecurity rate: 9.0%,</p>", 40.753079, 360-74.048066]
+   ]
+   function pick(index){
+     document.getElementById("display").innerHTML=funFacts[index][0];
+   }
